cee205a39cde782883a5f25b9f8164017a89e1b1
Mr. Bantsum <hdhdjdj7899@gmail.com>
2026-03-29 21:24:35 -0400

    conflicter
    jon

--- src/main/java/com/hbm/lib/RefStrings.java
+++ src/main/java/com/hbm/lib/RefStrings.java
@@ -2,13 +2,8 @@ package com.hbm.lib;
 
 public class RefStrings {
 	public static final String MODID = "hbm";
-<<<<<<< HEAD
 	public static final String NAME = "NTM: Space Copper Edition";
-	public static final String VERSION = "1.0.27 BETA (5645)";
-=======
-	public static final String NAME = "NTM: Space";
 	public static final String VERSION = "1.0.27 BETA (5649)";
->>>>>>> 12d94e20388a45606a05d9270da26b1e902c58c7
 	//HBM's Beta Naming Convention:
 	//V T (X)
 	//V -> next release version
